a8b1a837716a750099ae9b50afb3bd08937e49a5
Bryan Riveros <bryanrrs@gmail.com>
2021-10-02 13:42:03 -0300

    simple if functionality

--- src/functionsTags.rb
+++ src/functionsTags.rb
@@ -5,10 +5,7 @@ class ForTag
     end
 
     def render(context)
-        # pp context
-        # pp @forDesc[-1]
         retVal = context_lookup(context, @forDesc[-1] )
-        # pp retVal
         if retVal.nil?
             return ''
         else
@@ -31,4 +28,69 @@ class ForTag
 end
 
 class IfTag
+    def initialize(condition, nodes)
+        @condition = condition
+        @nodes = nodes
+    end
+
+    def render(context)
+        r = false 
+        case @condition.size
+        when 1
+            v = var_resolver(context, @condition[0])
+            if not v.nil?
+                r = v
+            end
+        when 3
+            v1 = var_resolver(context, @condition[0])
+            v2 = var_resolver(context, @condition[2])
+            if not v1.nil? and not v2.nil?
+                r = boolean_solver(v1, v2, @condition[1])
+            end
+        end
+        if r
+            @nodes.renderPage(context)
+        end
+        return nil
+    end
+
+    def var_resolver(context, var)
+        begin
+            val = Integer(var)
+        rescue ArgumentError
+            pp var
+            if var.downcase == 'false'
+                val = false
+            elsif var.downcase == 'true'
+                val = true
+            else
+                splited = var.split('.')
+                val = context
+                for s in splited
+                    val = val[s]
+                end
+            end
+        end
+        return val
+    end
+
+    def boolean_solver(v1, v2, cond)
+        v = false
+        begin
+            case cond
+            when '<'
+                v = v1 < v2
+            when '<='
+                v = v1 <= v2
+            when '>'
+                v = v1 > v2
+            when '>='
+                v = v1 >= v2
+            when '=='
+                v = v1 == v2
+            end
+        rescue ArgumentError
+            return v
+        end
+    end
 end
